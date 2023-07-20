561e6eafcf55003e1f44d16628b50ddd082a8c33
Italo_Roberto <60036677+Italo-Roberto@users.noreply.github.com>
2023-07-20 08:53:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,52 +3,52 @@ import datetime
 
 def scanner():
     print('''
-    ########### Scanner de portas UDP/ TCP ############
+    ########### Port scanner for UDP/ TCP ############
     ===================================================
-    Versão: 1.0.0
-    Link do respositório: https://github.com/Italo-Roberto/port_scanner_python
-    Autor: Italo Roberto
-    Data de criação: 04/10/2022
+    Version: 1.0.0
+    Repository link: https://github.com/Italo-Roberto/port_scanner_python
+    Author: Italo Roberto
+    Created at: 04/10/2022
     ===================================================
     ''')
-    alvo = input('Digite o IP/ Domínio: ')
+    target = input('Digite o IP/ Domínio: ')
 
-    portas = []
-    for porta in range(1, 995):
+    ports = []
+    for port in range(1, 995):
         client = socket.socket()
         client.settimeout(0.05)
 
-        if client.connect_ex((alvo, porta)) == 0:
-            print(f'Porta aberta: {porta}')
-            portas.append(porta)
-    print('Scanner finalizado! \n')
+        if client.connect_ex((target, port)) == 0:
+            print(f'Porta aberta: {port}')
+            ports.append(port)
+    print('Finnish Scan! \n')
 
-    pergunta = input('''
+    ask = input('''
     ===============================================================================
     Deseja salvar a lista de portas encontradas em arquivo de texto na pasta atual?\n
     s=SIM, n=Não
     ===============================================================================
     ''')
-    str(pergunta)
+    str(ask)
 
-    if pergunta == 's':
-        dataAtual = datetime.datetime.now()
-        dataFormatada = dataAtual.strftime('%d_%m_%Y-%H-%M')
-        arquivo = open(f"RelatorioDePortas{dataFormatada}.txt", "x")
-        arquivo.write(f'''
+    if ask == 's':
+        currentDate = datetime.datetime.now()
+        formatDate = currentDate.strftime('%d_%m_%Y-%H-%M')
+        file = open(f"RelatorioDePortas{formatDate}.txt", "x")
+        file.write(f'''
 ===========================
-Lista de portas abertas:
+Open ports list:
 ===========================
-HOST: {alvo}
-DATA DO SCANNER: {dataAtual.strftime('%c')}
+HOST: {target}
+DATA DO SCANNER: {currentDate.strftime('%c')}
         ''')
-        for p in portas:
-            arquivo.write(f'\n{p}')
-        arquivo.close()
-    elif pergunta == 'n':
-        print('Scanner de porta finalizado!')
+        for p in ports:
+            file.write(f'\n{p}')
+        file.close()
+    elif ask == 'n':
+        print('Port scanner finnished!')
     else:
-        print('Opção inválida, programa finalizado')
+        print('OInvalid option, program closed')
 
 
 if __name__ == '__main__':
